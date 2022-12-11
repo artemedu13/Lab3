e602f4e5f04df8cd9a53ec67aff0f615bff116b0
artemedu13 <119142239+artemedu13@users.noreply.github.com>
2022-12-11 15:21:36 +0300

    Лабораторная_работа_5

---
+++ Лабораторная работа 5/task5.py
@@ -0,0 +1,10 @@
+import random
+symbl = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digits = "0123456789"
+password = symbl + digits
+
+def get_random_password(n) -> str:
+    return "".join(random.sample(password, n))
+
+print(get_random_password(8))
+#Ответ
